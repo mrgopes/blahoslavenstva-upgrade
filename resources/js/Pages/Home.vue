5e75f3d7f0fb2f229f8a8a27d84ab277abf542b6
Michal Blava <michal.blava@gmail.com>
2021-11-17 12:01:50 +0100

    initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>hey</div>
+    <Link href="/test">test</Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    components: {
+        Link,
+    },
+    props: ["test"],
+};
+</script>
+
+<style scoped>
+
+</style>
