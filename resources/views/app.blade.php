27e48a2b855be0b11dc90211ae4b6cf3acf7e4f6
Michal Blava <michal.blava@gmail.com>
2021-11-24 19:59:43 +0100

    changed title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    <title>oŽI UPGRADE online</title>
+    <title>oŽI Upgrade ONLINE</title>
 
     <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
     <script>
